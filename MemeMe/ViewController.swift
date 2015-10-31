2ee60936c842a03ef1915e9e70867ceab1bb37cd
Narasimha Bhat <narasimhabhat@narasimhas-mac-mini.local>
2015-10-31 23:16:15 +0530

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Narasimha Bhat on 31/10/15.
+//  Copyright © 2015 Narasimha Bhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
